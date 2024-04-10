13161087754f107fedba21ed2d63bdbccce19401
mrinalmagar <109026918+mrinalmagar@users.noreply.github.com>
2024-04-10 15:11:52 +0200

    Update demo.sh

--- DEMO/demo.sh
+++ DEMO/demo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Launch Flask Server in a new terminal
-gnome-terminal -- /bin/bash -c 'cd /home/moro/ros2web/flask-app; 
+gnome-terminal -- /bin/bash -c 'cd /home/cafelatte/ros2web/flask-app; 
 echo "Starting Flask Server..."; 
 flask --app latency_test run --host 0.0.0.0; 
 exec /bin/bash'
@@ -14,7 +14,7 @@ exec /bin/bash'
 
 # Source ROS and run ROS package in a new terminal
 gnome-terminal -- /bin/bash -c 'source /opt/ros/humble/setup.bash; 
-cd /home/moro/humble_ws;
+cd /home/cafelatte/humble_ws;
 colcon build --packages-select sequence_controller;
 source install/local_setup.bash;
 ros2 run sequence_controller sequence;
@@ -23,13 +23,19 @@ exec /bin/bash'
 
 # Source micro-ros and run micro-ros package in a new terminal
 gnome-terminal -- /bin/bash -c 'source /opt/ros/humble/setup.bash; 
-cd /home/moro/microros_ws;
+cd /home/cafelatte/microros_ws;
 source install/local_setup.bash;
 echo "Launching micro_ros Agent..."; 
 ros2 run micro_ros_agent micro_ros_agent serial --dev /dev/ttyACM0;
 exec /bin/bash'
 
-sudo apt-get install ros-<ros2-distro>-image-transport ros-<ros2-distro>-image-transport-plugins
-ros2 run image_tools cam2image --ros-args -p device_id:=<your_device_id>
-ros2 run image_transport republish raw in:=/image_raw compressed out:=/image_raw/compressed
+
+# Source micro-ros and run camera-ros in a new terminal
+gnome-terminal -- /bin/bash -c 'source /opt/ros/humble/setup.bash; 
+ros2 run image_tools cam2image --ros-args -p device_id:=0;
+exec /bin/bash'
+
+gnome-terminal -- /bin/bash -c 'source /opt/ros/humble/setup.bash; 
+ros2 run image_transport republish raw in:=/image compressed out:=/image/compressed;
+exec /bin/bash'
 
